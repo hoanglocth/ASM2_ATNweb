fc9ab4f5b0845df9dd60292f8d8f1e1eccfcb632
HoangXuanLoc <hoanglocth@gmail.com>
2021-06-25 22:16:49 +0700

    update

---
+++ admin.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+    <link rel="stylesheet" href ="/css/style.css">
+    <title> Database </title>
+    <marquee bgcolor="gray" 
+             scrollamount="12" 
+             direction="left" 
+             onmouseover="this.stop();" 
+             onmouseout="this.start();">
+        <center>
+		<?php
+		date_default_timezone_set('Asia/Ho_Chi_Minh');
+		$date = getdate();
+			echo "<hr>";
+			echo "Today is: ".$date['weekday']."--".$date['mday']."/".$date['mon']."/".$date['year']."--".$date['hours'].":".$date['minutes'].":".$date['seconds'];
+		?>
+	  </center>
+    </marquee>
+    <ul>
+      <li> <a href="datashop1.php">VIEW DATABASE OF ATN SHOP 1</a> </li>
+		  <li> <a href="datashop2.php">VIEW DATABASE OF ATN SHOP 2</a> </li>
+	    <li> <a href="login.php">LOG OUT</a> </li>
+    </ul>
+  </head>
+    <body>
+      <style>
+        body {
+          background-image: url('background.jpg');
+          background-attachment: fixed;
+          background-size: 100%100%;
+        } 
+      </style>		 
+	</body>
+</html>
---
+++ delete1.php
@@ -0,0 +1,23 @@
+<?php
+$host_heroku = "ec2-34-195-143-54.compute-1.amazonaws.com";
+$db_heroku = "dc093vuc6gjaav";
+$user_heroku = "ubniakvyleptee";
+$pw_heroku = "237d8b8b0a1597a3f29b43a35aa8b02b413148386f81c7210be32fe2baaf259e";
+$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+$pg_heroku = pg_connect($conn_string);
+
+if (!$pg_heroku) {
+	die('Error: Could not connect: ' . pg_last_error());
+}
+$productid = $_GET['pi'];
+$query = "DELETE FROM atnshop1 WHERE productid = '$productid'";
+$data = pg_query($pg_heroku, $query);
+if ($data) {
+	echo "<script>alert('Delete Successfully!')</script>";
+?>
+	<meta http-equiv="refresh" content="0; url=https://asm-atn-shop.herokuapp.com/shop1.php" />
+<?php
+} else {
+	echo "Failed to delete.";
+}
+?>
